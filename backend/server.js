c0209877f587d9ae4e809a293eb13c700b22639f
Gregory Anderson <anderson.gregory.mail@gmail.com>
2021-01-08 13:40:55 -0700

    Add custom error handling

--- backend/server.js
+++ backend/server.js
@@ -1,21 +1,31 @@
-import express from 'express'
-import dotenv from 'dotenv'
-import colors from 'colors'
-import connectDB from './config/db.js'
-import productRouter from './routes/productRoutes.js'
+import express from "express";
+import dotenv from "dotenv";
+import colors from "colors";
+import { notFound, errorHandler } from './middleware/errorMiddleware.js'
+import connectDB from "./config/db.js";
+import productRouter from "./routes/productRoutes.js";
 
-dotenv.config()
+dotenv.config();
 
-connectDB()
+connectDB();
 
-const app = express()
+const app = express();
 
 app.get("/", (req, res) => {
-  res.send('API is running')
-})
+  res.send("API is running");
+});
 
-app.use('/api/products', productRouter)
+app.use("/api/products", productRouter);
 
-const PORT = process.env.PORT || 5000
+app.use(notFound);
 
-app.listen(PORT, console.log(`Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold))
+app.use(errorHandler);
+
+const PORT = process.env.PORT || 5000;
+
+app.listen(
+  PORT,
+  console.log(
+    `Server running in ${process.env.NODE_ENV} on port ${PORT}`.yellow.bold
+  )
+);
