8d73b5eac5cf79238480451d5ac9ab9216661170
Gregory Anderson <anderson.gregory.mail@gmail.com>
2021-01-12 14:02:55 -0700

    Add update user profile route

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -30,8 +30,6 @@ const authUser = asyncHandler(async (req, res) => {
 const registerNewUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
 
-  console.log("lalal", name, email, password);
-
   const userExists = await User.findOne({ email });
 
   if (userExists) {
@@ -74,4 +72,32 @@ const getUserProfile = asyncHandler(async (req, res) => {
   }
 });
 
-export { authUser, getUserProfile, registerNewUser };
+// @desc UPDATE user profile
+// @route PUT api/users/profile
+// @access private
+const updateUserProfile = asyncHandler(async (req, res) => {
+  const user = await User.findById(req.user._id);
+
+  if (user) {
+    user.name = req.body.name || user.name;
+    user.email = req.body.email || user.email;
+    if (req.body.password) {
+      user.password = req.body.password || user.password;
+    }
+
+    const updatedUser = await user.save();
+
+    res.json({
+      _id: updatedUser._id,
+      name: updatedUser.name,
+      email: updatedUser.email,
+      isAdmin: updatedUser.isAdmin,
+      token: generateToken(updatedUser._id),
+    });
+  } else {
+    res.status(404);
+    throw new Error("User not found");
+  }
+});
+
+export { authUser, getUserProfile, registerNewUser, updateUserProfile };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,15 +1,19 @@
 import express from "express";
 import {
-authUser,
-getUserProfile,
-registerNewUser
+  authUser,
+  getUserProfile,
+  registerNewUser,
+  updateUserProfile,
 } from "../controllers/userController.js";
-import { protect } from '../middleware/authMiddleware.js'
+import { protect } from "../middleware/authMiddleware.js";
 
 const router = express.Router();
 
 router.route("/").post(registerNewUser);
 router.post("/login", authUser);
-router.route("/profile").get(protect, getUserProfile);
+router
+  .route("/profile")
+  .get(protect, getUserProfile)
+  .put(protect, updateUserProfile);
 
 export default router;
