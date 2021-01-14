24a4381ef70dffc0339c4d2fe7854259b3fac503
Gregory Anderson <anderson.gregory.mail@gmail.com>
2021-01-13 17:06:57 -0700

    Add shipping screen, reducer, and action

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -9,6 +9,7 @@ import CartScreen from "./screens/CartScreen";
 import LoginScreen from "./screens/LoginScreen";
 import RegisterScreen from "./screens/RegisterScreen";
 import ProfileScreen from "./screens/ProfileScreen";
+import ShippingScreen from "./screens/ShippingScreen";
 
 import "./App.css";
 
@@ -18,6 +19,7 @@ const App = () => {
       <Header />
       <main className="py-3">
         <Container>
+          <Route path="/shipping" component={ShippingScreen} />
           <Route path="/profile" component={ProfileScreen} />
           <Route path="/register" component={RegisterScreen} />
           <Route path="/login" component={LoginScreen} />
