94bb418939cc012cc0cbadc2554165c04b4102e1
Gregory Anderson <anderson.gregory.mail@gmail.com>
2021-01-18 19:27:06 -0700

    Add meta component

---
+++ frontend/src/components/Meta.js
@@ -0,0 +1,20 @@
+import React from "react";
+import { Helmet } from "react-helmet";
+
+const Meta = ({ title, description, keywords }) => {
+  return (
+    <Helmet>
+      <title>{title}</title>
+      <meta name="description" content={description} />
+      <meta name="keyword" content={keywords} />
+    </Helmet>
+  );
+};
+
+Meta.defaultProps = {
+  title: "Buttermilk's Bodega",
+  description: "How can we help you?",
+  keywords: "electronics, buy electronics, cheap electronics",
+};
+
+export default Meta;
